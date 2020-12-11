222eb0b3996a6d6efedbe4157ef2c958cd99fa23
Munir Mahmud <munirmahmud6@gmail.com>
2020-12-11 16:44:20 +0600

    Changed class component to functional &  set toggle info

--- src/components/TourLists/Tour/Tour.js
+++ src/components/TourLists/Tour/Tour.js
@@ -1,31 +1,37 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { FaCaretSquareDown, FaWindowClose } from 'react-icons/fa';
 import './tour.scss';
 
-export default class Tour extends Component {
-    render() {
-        const {id, city, img, name, info} = this.props.tour;
-        const { removeTour } = this.props.removeTour;
-        
-        return (
-            <article className="tour">
-                <div className="img__container">
-                    <img src={img} alt={name} />
-                
-                    <span className="close__btn">
-                        <FaWindowClose />
-                    </span>
-                </div>
+const Tour = (props) => {
+    const [showInfo, setShowInfo] = useState(false);
 
-                <div className="tour__info">
-                    <h3>{city}</h3>
-                    <h4>{name}</h4>
-                    <h5>Info <FaCaretSquareDown className="icon__down" /></h5>
+    const {id, city, img, name, info} = props.tour;
+    const { removeTour } = props.removeTour;
 
-                    <p>{info}</p>
-                </div>
-                
-            </article>
-        )
-    }
+    const toggleInfo = () => {
+        setShowInfo(showInfo => !showInfo);
+    };
+    
+    return (
+        <article className="tour">
+            <div className="img__container">
+                <img src={img} alt={name} />
+            
+                <span className="close__btn">
+                    <FaWindowClose />
+                </span>
+            </div>
+
+            <div className="tour__info">
+                <h3>{city}</h3>
+                <h4>{name}</h4>
+                <h5>Info <FaCaretSquareDown onClick={toggleInfo} className="icon__down" /></h5>
+
+                {showInfo && <p>{info}</p>}
+            </div>
+            
+        </article>
+    )
 }
+
+export default Tour;
